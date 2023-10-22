947c0d8842b3f9a4130d0355a4ef9757e3f96492
Kursat Kilic <kilic_kursat@hotmail.com>
2023-10-22 17:52:38 +0900

    Update TBM-EDA.py

--- TBM-EDA.py
+++ TBM-EDA.py
@@ -1,54 +1,56 @@
-import pickle
+import numpy as np
 import pandas as pd
 import streamlit as st
+#from pandas_profiling import ProfileReport
 from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
-def load_data(mode='online'):
-  """Loads data from a CSV or Excel file.
+# Web App Title
+st.markdown('''
+# **The EDA App**
 
-  Args:
-    mode: The data loading mode. Can be either 'online' or 'batch'.
+This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
 
-  Returns:
-    A Pandas DataFrame containing the loaded data.
-  """
+**Credit:** App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
 
-  if mode == 'online':
-    # Load data from an uploaded file
-    uploaded_file = st.file_uploader('Upload a CSV or Excel file')
-    if uploaded_file is None:
-      return None
-    else:
-      return pd.read_csv(uploaded_file) if uploaded_file.name.endswith('.csv') else pd.read_excel(uploaded_file)
+---
+''')
 
-  elif mode == 'batch':
-    # Load data from a pre-existing file
-    file_path = st.text_input('Enter the path to the CSV or Excel file')
-    if file_path is None or file_path == '':
-      return None
-    else:
-      return pd.read_csv(file_path) if file_path.endswith('.csv') else pd.read_excel(file_path)
+# Upload CSV data
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+""")
 
-  else:
-    raise ValueError('Invalid data loading mode: {}'.format(mode))
-
-# Load the data
-df = load_data(mode='online')
-
-# Pickle the data
-with open('data.pickle', 'wb') as f:
-  pickle.dump(df, f)
-
-# Load the pickled data
-with open('data.pickle', 'rb') as f:
-  df = pickle.load(f)
-
-# Create a ydata-profiling report
-report = ProfileReport(df)
-st_profile_report(report)
-
-
-# Display the report in Streamlit
-st.title('TBM EDA Report')
-st.write(report.to_html())
+# Pandas Profiling Report
+if uploaded_file is not None:
+    @st.cache_data
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        # Example data
+        @st.cache_data
+        def load_data():
+            a = pd.DataFrame(
+                np.random.rand(100, 5),
+                columns=['a', 'b', 'c', 'd', 'e']
+            )
+            return a
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
