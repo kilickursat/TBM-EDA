f9b7212a1eacab4a59a048038468908c3f75451b
Kursat Kilic <kilic_kursat@hotmail.com>
2023-10-22 18:22:48 +0900

    Update TBM-EDA.py

--- TBM-EDA.py
+++ TBM-EDA.py
@@ -20,76 +20,73 @@ data_loading_option = st.radio("Select data loading option:", ("Online Data", "B
 
 # Online Data Loading
 if data_loading_option == "Online Data":
-  with st.sidebar:
-    st.header('Online Data Loading')
-    uploaded_file = st.file_uploader("Upload your input file (CSV or Excel)", type=["csv", "xlsx"])
-    st.markdown("""
-    [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
-    """)
+    with st.sidebar:
+        st.header('Online Data Loading')
+        uploaded_file = st.file_uploader("Upload your input file (CSV or Excel)", type=["csv", "xlsx"])
+        st.markdown("""
+        [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+        """)
 
-  if uploaded_file is not None:
-    @st.cache
-    def load_data(file):
-      if file.name.endswith('.csv'):
-        return pd.read_csv(file)
-      elif file.name.endswith('.xlsx'):
-        return pd.read_excel(file, engine='openpyxl')
+    if uploaded_file is not None:
+        @st.cache
+        def load_data(file):
+            if file.name.endswith('.csv'):
+                return pd.read_csv(file)
+            elif file.name.endswith('.xlsx'):
+                return pd.read_excel(file, engine='openpyxl')
 
-    df = load_data(uploaded_file)
-    pr = ProfileReport(df, explorative=True)
+        df = load_data(uploaded_file)
+        pr = ProfileReport(df, explorative=True)
 
-    st.header('**Input DataFrame**')
-    st.write(df)
-    st.write('---')
-    # Add the following code to center the results and EDA in the middle of the page
-    st.markdown("""
-    <style>
-    .st-content {
-      margin: auto;
-      width: 1000px;
-    }
-    </style>
-    """)
-
-    st.header('**Pandas Profiling Report**')
-    st_profile_report(pr)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
 
 # Batch Data Loading
 if data_loading_option == "Batch Data":
-  with st.sidebar:
-    st.header('Batch Data Loading')
-    st.markdown("You can choose to load an example dataset or upload your own CSV or Excel file.")
-    example_button = st.button('Use Example Dataset')
+    with st.sidebar:
+        st.header('Batch Data Loading')
+        st.markdown("You can choose to load an example dataset or upload your own CSV or Excel file.")
+        example_button = st.button('Use Example Dataset')
 
-    if example_button:
-      # Example data
-      @st.cache
-      def load_example_data():
-        a = pd.DataFrame(
-          np.random.rand(100, 5),
-          columns=['a', 'b', 'c', 'd', 'e']
-        )
-        return a
+        if example_button:
+            # Example data
+            @st.cache
+            def load_example_data():
+                a = pd.DataFrame(
+                    np.random.rand(100, 5),
+                    columns=['a', 'b', 'c', 'd', 'e']
+                )
+                return a
 
-      df = load_example_data()
-      pr = ProfileReport(df, explorative=True)
+            df = load_example_data()
+            pr = ProfileReport(df, explorative=True)
 
-      st.header('**Input DataFrame**')
-      st.write(df)
-      st.write('---')
-    # Add the following code to center the results and EDA in the middle of the page
-    st.markdown("""
-    <style>
-    .st-content {
-      margin: auto;
-      width: 1000px;
-    }
-    </style>
-    """)
+            st.header('**Input DataFrame**')
+            st.write(df)
+            st.header('**Pandas Profiling Report**')
+            st_profile_report(pr)
 
-      st.header('**Pandas Profiling Report**')
-      st_profile_report(pr)
+        else:
+            uploaded_file = st.file_uploader("Upload your input file (CSV or Excel)", type=["csv", "xlsx"])
+            st.markdown("""
+            [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+            """)
 
-    else:
-      uploaded_file = st.file_uploader("Upload your input file (CSV or Excel)", type=["csv", "xlsx"])
-      
+            if uploaded_file is not None:
+                @st.cache
+                def load_data(file):
+                    if file.name.endswith('.csv'):
+                        return pd.read_csv(file)
+                    elif file.name.endswith('.xlsx'):
+                        return pd.read_excel(file, engine='openpyxl')
+
+                df = load_data(uploaded_file)
+                pr = ProfileReport(df, explorative=True)
+
+                st.header('**Input DataFrame**')
+                st.write(df)
+                st.header('**Pandas Profiling Report**')
+                st_profile_report(pr)
