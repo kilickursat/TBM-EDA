b597d5de70a8d8a1d48e21cd9980499dd4c9ab5f
Kursat Kilic <kilic_kursat@hotmail.com>
2023-10-22 18:36:53 +0900

    Update TBM-EDA.py

--- TBM-EDA.py
+++ TBM-EDA.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import streamlit as st
 from ydata_profiling import ProfileReport
@@ -22,38 +21,30 @@ data_loading_option = st.radio("Select data loading option:", ("Online Data", "B
 if data_loading_option == "Online Data":
     with st.sidebar:
         st.header('Online Data Loading')
-        uploaded_file = st.file_uploader("Upload your input file (CSV or Excel)", type=["csv", "xlsx"])
-        st.markdown("""
-        [Example CSV input file](https://github.com/kilickursat/WebApp/blob/main/TBM_Performance.xlsx)
-        """)
+        st.markdown("You can choose to load the online dataset or upload your own CSV or Excel file.")
+        st.markdown("[Example Online Dataset](https://github.com/kilickursat/WebApp/blob/main/TBM_Performance.xlsx)")
+        online_button = st.button('Use Online Dataset')
 
-    if uploaded_file is not None:
-        @st.cache
-        def load_data(file):
-            if file.name.endswith('.csv'):
-                return pd.read_csv(file)
-            elif file.name.endswith('.xlsx'):
-                return pd.read_excel(file, engine='openpyxl')
+        if online_button:
+            # Load the online dataset
+            online_data_link = "https://github.com/kilickursat/WebApp/raw/main/TBM_Performance.xlsx"
+            df = pd.read_excel(online_data_link, engine='openpyxl')
+            pr = ProfileReport(df, explorative=True)
 
-        df = load_data(uploaded_file)
-        pr = ProfileReport(df, explorative=True)
-
-        st.header('**Input DataFrame**')
-        st.write(df)
-        st.write('---')
-        st.header('**Pandas Profiling Report**')
-        st_profile_report(pr)
+            st.header('**Input DataFrame**')
+            st.write(df)
+            st.write('---')
+            st.header('**Pandas Profiling Report**')
+            st_profile_report(pr)
 
 # Batch Data Loading
 if data_loading_option == "Batch Data":
     with st.sidebar:
         st.header('Batch Data Loading')
         st.markdown("You can choose to upload your own CSV or Excel file.")
-
+    
     uploaded_file = st.file_uploader("Upload your input file (CSV or Excel)", type=["csv", "xlsx"])
-    st.markdown("""
-    [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
-    """)
+    st.markdown("[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)")
 
     if uploaded_file is not None:
         @st.cache
