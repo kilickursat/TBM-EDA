a733196a3c93e29d558b269e30216f369ddd97a4
Kursat Kilic <kilic_kursat@hotmail.com>
2023-10-27 17:02:56 +0900

    Update TBM-EDA-ML.py

--- TBM-EDA-ML.py
+++ TBM-EDA-ML.py
@@ -104,7 +104,60 @@ if data_loading_option == "Batch Data":
 
         st.write(f'Mean Squared Error: {mse}')
         st.write(f'R-squared: {r2}')
-      
+        # Plot Predicted vs. Actual
+        st.header('Predicted vs. Actual Values')
+
+        plt.scatter(y_test, y_pred)
+        plt.xlabel('Actual Values')
+        plt.ylabel('Predicted Values')
+        plt.title('Predicted vs. Actual Values')
+        st.pyplot()
+
+        # Plot Learning Curve
+        st.header('Learning Curve')
+
+# Define a function to plot the learning curve
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    if axes is None:
+        _, axes = plt.subplots(1, 1, figsize=(5, 5))
+
+    axes.set_title(title)
+    if ylim is not None:
+        axes.set_ylim(*ylim)
+    axes.set_xlabel("Training examples")
+    axes.set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
+                       train_sizes=train_sizes,
+                       return_times=True)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes.grid()
+    axes.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    axes.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1,
+                     color="g")
+    axes.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    axes.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+    axes.legend(loc="best")
+    plt.show()
+
+# Plot the learning curve
+plot_learning_curve(model, "Learning Curve", X, y)
+st.pyplot()
+    
 st.link_button("Go to pyWalker", "https://docs.kanaries.net/pygwalker")
 st.link_button("Go to pandas-profiling", "https://github.com/ydataai/ydata-profiling")
 
