9de9cb0501d412cb032521adea44f3b495ca2d98
Kursat Kilic <kilic_kursat@hotmail.com>
2023-10-27 17:07:58 +0900

    Update TBM-EDA-ML.py

--- TBM-EDA-ML.py
+++ TBM-EDA-ML.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
+import matplotlib.pyplot as plt
 # Set page layout to 'wide'
 st.set_page_config(layout='wide')
 
