ad760eb52cef612aa7c1f85b4e1b8d02979a3d86
Kursat Kilic <kilic_kursat@hotmail.com>
2023-10-27 16:21:54 +0900

    Update TBM-EDA.py

--- TBM-EDA.py
+++ TBM-EDA.py
@@ -5,31 +5,31 @@ from streamlit_pandas_profiling import st_profile_report
 from PIL import Image
 import pygwalker as pyg
 import streamlit.components.v1 as components
-col1, col2, col3 =st.columns(3)
 
-#st.set_page_config(layout='wide')
+col1, col2, col3 = st.columns(3)
+
+# Set up the layout in the first column (col1)
 with col1:
+    st.set_page_config(layout='wide')
+    
     image = Image.open('Kursat_Artificial_intelligence_and_TBM.png')
-
     st.success('Welcome!')
-
-    st.image(image, caption='Intelligent-TBM-by-Midjourney',width=500)
-    # Add two JPG images, one aligned left and the other aligned right
-    # Web App Title
+    st.image(image, caption='Intelligent-TBM-by-Midjourney', width=500)
+    
     st.markdown('''
-                    # **The TBM EDA App**
+        # **The TBM EDA App**
 
-                    This is the **TBM EDA App** created in Streamlit using the **pandas-profiling** and **pyWalker** libraries.
+        This is the **TBM EDA App** created in Streamlit using the **pandas-profiling** and **pyWalker** libraries.
 
-        **Credit:** App built in `Python` + `Streamlit Cloud` + `ChatGPT` + `pyWalker` by [Kursat Kilic](https://github.com/kilickursat) (Researcher for TUST&AI field))
+        **Credit:** App built in `Python` + `Streamlit Cloud` + `ChatGPT` + `pyWalker` by [Kursat Kilic](https://github.com/kilickursat) (Researcher for TUST&AI field)
 
         ---
-            ''')
-        # Option to choose online or batch data loading
+    ''')
+
     data_loading_option = st.radio("Select data loading option:", ("Online Data", "Batch Data"))
 
-# Online Data Loading
-
+# Define components for online data loading in the second column (col2)
+with col2:
     if data_loading_option == "Online Data":
         with st.sidebar:
             st.header('Press to use Online Data Loading')
@@ -37,38 +37,30 @@ with col1:
             st.markdown("[Example Online Dataset](https://github.com/kilickursat/WebApp/blob/main/TBM_Performance.xlsx)")
             online_button = st.button('Press to Use Online Dataset')
 
-            if online_button:
-                # Load the online dataset
-                online_data_link = "https://github.com/kilickursat/WebApp/raw/main/TBM_Performance.xlsx"
-                df = pd.read_excel(online_data_link, engine='openpyxl')
-            
-                # Show the data as a table 
-                st.dataframe(df)
-                # Show statistics on the data
-                st.write(df.describe())
-                with col2:
-                    pr = ProfileReport(df, explorative=True)
+        if online_button:
+            online_data_link = "https://github.com/kilickursat/WebApp/raw/main/TBM_Performance.xlsx"
+            df = pd.read_excel(online_data_link, engine='openpyxl')
+            st.dataframe(df)
+            st.write(df.describe())
 
-                    # Generate the HTML using PygWalker
-                    st.header('orange[pyWalker EDA]')
-                    st.markdown("This is the pyWalker. Please play with X-axis and Y-axis just doing drag and drop")
-                    pyg_html = pyg.to_html(df,hideDataSourceConfig=True,themekey="vega",dark="media")  # Replace 'pyg' with the correct pygWalker object
+            pr = ProfileReport(df, explorative=True)
+            st.header('orange[pyWalker EDA]')
+            st.markdown("This is the pyWalker. Please play with X-axis and Y-axis just by doing drag and drop")
+            pyg_html = pyg.to_html(df, hideDataSourceConfig=True, themekey="vega", dark="media")
+            components.html(pyg_html, height=1000, scrolling=True)
+            st.header('**Input DataFrame**')
+            st.write(df)
+            st.write('---')
+            st.header('**Pandas Profiling Report**')
+            st_profile_report(pr)
 
-                    # Embed the HTML into the Streamlit app
-                    components.html(pyg_html, height=1000, scrolling=True)
-
-                    st.header('**Input DataFrame**')
-                    st.write(df)
-                    st.write('---')
-                    st.header('**Pandas Profiling Report**')
-                    st_profile_report(pr)
+# Define components for batch data loading in the third column (col3)
 with col3:
-    # Batch Data Loading
     if data_loading_option == "Batch Data":
         with st.sidebar:
             st.header('Batch Data Loading')
             st.markdown("You can choose to upload your own CSV or Excel file.")
-    
+
         uploaded_file = st.file_uploader("Upload your input file (CSV or Excel)", type=["csv", "xlsx"])
         st.markdown("[Example excel input file](https://github.com/kilickursat/WebApp/raw/main/TBM_Performance.xlsx)")
 
@@ -83,24 +75,15 @@ with col3:
             df = load_data(uploaded_file)
             st.dataframe(df)
             st.write(df.describe())
-        
-            
+
             pr = ProfileReport(df, explorative=True)
-            # Generate the HTML using PygWalker
             st.header('pyWalker Page')
-            st.markdown("This is the pyWalker. Please play with X-axis and Y-axis just doing drag and drop")
-            pyg_html = pyg.to_html(df,hideDataSourceConfig=True,themekey="vega",dark="media")  # Replace 'pyg' with the correct pygWalker object
-        
-            # Embed the HTML into the Streamlit app
+            st.markdown("This is the pyWalker. Please play with X-axis and Y-axis just by doing drag and drop")
+            pyg_html = pyg.to_html(df, hideDataSourceConfig=True, themekey="vega", dark="media")
             components.html(pyg_html, height=1000, scrolling=True)
 
-            # Embed the HTML into the Streamlit app
-            #components.html(pyg_html, height=1000, scrolling=True)
             st.header('**Input DataFrame**')
             st.write(df)
-            st.header('**Pandas Profiling Report**')
-            st_profile_report(pr)
-        
-st.link_button("Go to pyWalker", "https://docs.kanaries.net/pygwalker")
-st.link_button("Go to pandas-profiling","https://github.com/ydataai/ydata-profiling")
 
+st.link_button("Go to pyWalker", "https://docs.kanaries.net/pygwalker")
+st.link_button("Go to pandas-profiling", "https://github.com/ydataai/ydata-profiling")
