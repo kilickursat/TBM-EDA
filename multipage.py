8a4f0fa47f48c206f69a78a66d534c9c4ef6f354
Kursat Kilic <kilic_kursat@hotmail.com>
2023-11-24 01:47:43 +0900

    Update multipage.py

--- multipage.py
+++ multipage.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Set page layout to 'wide'
 st.set_page_config(layout='wide')
